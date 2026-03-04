acd49eefa05106a93bd729bce631328c403a4be7
daniloldn <112765545+daniloldn@users.noreply.github.com>
2026-03-04 15:23:19 +0000

    policy

---
+++ src/policy.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+
+
+def run_random(bandit, T=200):
+    """Run a random policy for T rounds.
+
+    Returns:
+    rewards: array of length T, reward at each round (0 or 1)
+    arms_played: array of length T, index of arm chosen each round
+"""
+    
+
+
+    return None
