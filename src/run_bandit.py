b6a27f0d52b08f2986e48445b10431164ec91f88
daniloldn <112765545+daniloldn@users.noreply.github.com>
2026-03-04 16:25:19 +0000

    starting Q1b

--- src/run_bandit.py
+++ src/run_bandit.py
@@ -2,10 +2,11 @@ from src.bandit_env import AdChannelBandit
 from src.policy import run_random
 from src.metrics import total_rewards, regret
 import plotly.express as px
+import numpy as np
 
 
 
-def simulate():
+def simulate(model):
 
     #bandit init
     bandit = AdChannelBandit()
@@ -13,23 +14,33 @@ def simulate():
     #time periods
     T=2000
 
-    #run policy 
-    results_dict = run_random(bandit, T)
+    all_rewards = np.empty((50, T), dtype=float)
+    all_regrets = np.empty((50, T), dtype=float)
 
-    rewards = total_rewards(results_dict["rewards"])
-    regrets= regret(bandit, results_dict["arms"])
+    #run policy 
+    for i in range(50):
+        results_dict = model(bandit, T)
+        rewards = total_rewards(results_dict["rewards"])
+        regrets= regret(bandit, results_dict["arms"])
+
+        #storing results
+        all_rewards[i, :] = rewards
+        all_regrets[i, :] = regrets
+
+    avg_reward = all_rewards.mean(axis=0)
+    avg_regret = all_regrets.mean(axis=0)
 
     fig1 = px.line(
-    x=range(len(rewards)),
-    y=rewards,
+    x=range(len(avg_reward)),
+    y=avg_reward,
     title="Random policy: cumulative reward",
     labels={"x": "t", "y": "cumulative reward"}
     )
     fig1.show()
 
     fig2 = px.line(
-    x=range(len(regrets)),
-    y=regrets,
+    x=range(len(avg_regret)),
+    y=avg_regret,
     title="Random policy: cumulative regret",
     labels={"x": "t", "y": "cumulative regret"}
     )
